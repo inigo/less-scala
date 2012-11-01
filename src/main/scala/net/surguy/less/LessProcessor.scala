67499830bacb2ee0165f361111efe2f0d4ec1d97
Inigo <inigo.surguy@gmail.com>
2012-11-01 09:23:00 +0000

    Use an optional partial function to transform the Less tree while it is being copied

--- src/main/scala/net/surguy/less/LessProcessor.scala
+++ src/main/scala/net/surguy/less/LessProcessor.scala
@@ -6,17 +6,26 @@ package net.surguy.less
  * @author Inigo Surguy
  */
 object LessProcessor {
+  private val NullPf: PartialFunction[Css, Css] = { case x if false => x }
 
-  // Should do with partial functions: http://weblogs.java.net/blog/cayhorstmann/archive/2010/05/16/transforming-xml-tree-scala-partial-functions
+  def process(css: Css, pf: PartialFunction[Css, Css] = NullPf): Css = new Transformer(pf).process(css)
+
   // @todo Do this with a Lens instead: http://stackoverflow.com/questions/3900307/cleaner-way-to-update-nested-structures/3900498#3900498
-  def process(css: Css): Css = css match {
-    case Stylesheet(directives, rules) => Stylesheet(directives.map(process).asInstanceOf[Seq[Directive]], rules.map(process).asInstanceOf[Seq[Ruleset]] )
-    case Directive(directive) => Directive(process(directive).asInstanceOf[DirectiveTerm])
-    case Ruleset(selector, declarations) => Ruleset(process(selector).asInstanceOf[Selector], declarations.map(process).asInstanceOf[Seq[Declaration]])
-    case Selector(terms) => Selector(terms.map(process).asInstanceOf[Seq[SelectorTerm]])
-    case Declaration(property, value) => Declaration(process(property).asInstanceOf[Property], process(value).asInstanceOf[Value])
-    case Variable(name, value) => Variable(process(name).asInstanceOf[VariableName], process(value).asInstanceOf[VariableValue])
-    case VariableOperation(val1, op, val2) => VariableOperation(process(val1).asInstanceOf[VariableValue], process(op).asInstanceOf[Operator], process(val2).asInstanceOf[VariableValue])
-    case terminal => terminal
+  class Transformer(pf: PartialFunction[Css, Css]) {
+    def process(css: Css): Css = {
+      css match {
+        case x if pf.isDefinedAt(x) => pf.apply(x)
+
+        case Stylesheet(directives, rules) => Stylesheet(directives.map(process).asInstanceOf[Seq[Directive]], rules.map(process).asInstanceOf[Seq[Ruleset]] )
+        case Directive(directive) => Directive(process(directive).asInstanceOf[DirectiveTerm])
+        case Ruleset(selector, declarations) => Ruleset(process(selector).asInstanceOf[Selector], declarations.map(process).asInstanceOf[Seq[Declaration]])
+        case Selector(terms) => Selector(terms.map(process).asInstanceOf[Seq[SelectorTerm]])
+        case Declaration(property, value) => Declaration(process(property).asInstanceOf[Property], process(value).asInstanceOf[Value])
+        case Variable(name, value) => Variable(process(name).asInstanceOf[VariableName], process(value).asInstanceOf[VariableValue])
+        case VariableOperation(val1, op, val2) => VariableOperation(process(val1).asInstanceOf[VariableValue], process(op).asInstanceOf[Operator], process(val2).asInstanceOf[VariableValue])
+        case terminal => terminal
+      }
+    }
   }
+
 }
