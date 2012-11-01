fb590d23f64580452c9ad2da2318d09e8ffa63ce
Inigo <inigo.surguy@gmail.com>
2012-11-01 09:35:40 +0000

    Test the partial function for transformation

--- src/test/scala/net/surguy/less/LessProcessorTest.scala
+++ src/test/scala/net/surguy/less/LessProcessorTest.scala
@@ -6,13 +6,14 @@ import org.specs2.mutable.Specification
  * @author Inigo Surguy
  */
 class LessProcessorTest extends Specification {
-
   import LessProcessor._
 
   "Processing a CSS tree with no Less" should {
-    "give the same output" in {
-      val css = new Ruleset(new Selector(List(SelectorTerm(".thing"))), List(new Declaration(new Property("color"), new SimpleValue("red"))))
-      process(css) mustEqual css
+    val simpleRuleset = new Ruleset(new Selector(List(SelectorTerm(".thing"))), List(new Declaration(new Property("color"), new SimpleValue("red"))))
+    "give the same output when there are no transformations" in { process(simpleRuleset) mustEqual simpleRuleset}
+    "use the partial function to transform specific values" in {
+      process(simpleRuleset, {case SelectorTerm(t) => SelectorTerm("changed") }) mustEqual
+        new Ruleset(new Selector(List(SelectorTerm("changed"))), List(new Declaration(new Property("color"), new SimpleValue("red"))))
     }
   }
 
